001a18d71eb775784151a101f9d4baa4cda827bf
Tamás Fogt <fogttamas@hotmail.com>
2016-06-15 11:58:24 +0200

    Environment setup 1

---
+++ storyline/controller/src/main/java/com/storylinetheapp/controller/User.java
@@ -0,0 +1,5 @@
+package com.storylinetheapp.controller;
+
+public class User {
+
+}
---
+++ storyline/service/src/main/java/com/storylinetheapp/service/App.java
@@ -0,0 +1,13 @@
+package com.storylinetheapp.service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
